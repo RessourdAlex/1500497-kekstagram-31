f9d5cf029136b586aa4bfa596494fc4c2acc257e
RessourdAlex <pichkuryra@gmail.com>
2024-04-05 01:08:59 +0300

    Функция времени

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,46 @@
-// const workingMeetings = (startWorkDay, finishWorkDay, startWorkMetting, finishWorkMetting) => {
+const workingMeetings = (...numbers) => {
+  const dateArray = [];
 
-// }
-function printCustomTime(timeString) {
-  let time = new Date(`2024-04-04T${timeString}`);
-  let hours = time.getHours();
-  let minutes = time.getMinutes();
-  console.log(`Заданное время: ${hours}:${minutes}`);
+  for (let j = 0; j <= 2; j += 1){
+    const checkFormatDate = numbers[j].split(':');
+
+    for (let i = 0; i <= 1; i += 1){
+      if (checkFormatDate[i].length === 1) {
+        checkFormatDate[i] = `0${checkFormatDate[i]}`;
+      }
+    }
+
+    numbers[j] = checkFormatDate.join(':');
+    const time = new Date(`2000-01-01T${numbers[j]}`);
+
+    dateArray.push(time);
+
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+
+    if (isNaN(`${hours}`) || isNaN(`${minutes}`)) {
+      throw new Error(`Недопустимый формат даты: "${numbers[j]}" !`);
+    }
+  }
+
+  const endMetting = dateArray[2];
+
+  endMetting.setMinutes(endMetting.getMinutes() + numbers[3]);
+
+  if (dateArray[0] <= dateArray[2] && dateArray[1] >= endMetting) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const startWorkDay = '9:00';
+const finishWorkDay = '18:00';
+const startWorkMetting = '15:06';
+const finishWorkMetting = 172;
+
+try{
+  console.log(workingMeetings(startWorkDay, finishWorkDay, startWorkMetting, finishWorkMetting));
+} catch (error){
+  console.error(error.message);
 }
-
-printCustomTime('15:30');
